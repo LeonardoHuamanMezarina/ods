ed93ccc3af28b3f011f3900d1d86437c553c86de
ordonezL1 <luis.ordonez.lopez@vallegrande.edu.pe>
2024-09-29 18:19:24 -0500

    feat: adding autoType

---
+++ JavaScript/autoType.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Fin de la pobreza", "Protejamos la Tierra", "Ciudades y comunidades sostenibles", "Mejorar la vida de todos"],
+    typeSpeed: 75,
+    backSpeed: 75,
+    loop: true
+  })
